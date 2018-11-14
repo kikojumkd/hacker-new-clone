c40eaaba445ac6bcb41116923b934fd10f7464c0
Kristijan <kristija.junuzovski@codelogiq.com>
2018-11-14 14:36:58 +0100

    initial UI and functionalities created

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'components/App';
+import { Provider } from 'react-redux';
+import configureStore from 'store';
+import setGlobalStyles from 'styles/globals';
+import * as serviceWorker from './serviceWorker';
+
+setGlobalStyles();
+
+const renderApp = () => {
+	const initialState = {};
+	const store = configureStore(initialState);
+
+	store.dispatch({ type: '@hnReader/@@INIT' });
+
+	ReactDOM.render(<Provider store={store}>
+		<App />
+		</Provider>,
+	document.getElementById('root')
+	);
+};
+
+renderApp();
+
+serviceWorker.unregister();
---
+++ src/styles/mediaQueries.js
@@ -0,0 +1,3 @@
+export const mobile = '@media only screeen and (max-width: 480px)';
+export const tablet = '@media only screeen and (max-width: 768px)';
+export const monitor = '@media only screeen and (min-width: 1400px)';
