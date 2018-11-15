74b75c677369a041d48e2870947182947c119828
Kristijan <kristija.junuzovski@codelogiq.com>
2018-11-15 12:59:55 +0100

    Added Infinite Scroll. Finalized the app.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import logo from './logo.svg';
 import { ThemeProvider } from 'styled-components';
 import List from 'components/List';
 import { colorsDark, colorsLight } from 'styles/palette';
+import Loader from 'components/Loader';
 
 import { Wrapper, Title } from './styles';
 
@@ -11,14 +13,32 @@ class App extends Component {
     this.props.fetchStoriesFirstPage();
   }
 
+  fetchStories = () => {
+    const { storyIds, page, fetchStories, isFetching } = this.props;
+    if (!isFetching) {
+      fetchStories({storyIds, page});
+    }
+  }
+
   render() {
-    const { stories } = this.props;
+    const { stories, hasMoreStories } = this.props;
     return (
       <ThemeProvider theme={colorsDark}>
         <div>
           <Wrapper>
             <Title>// Hacker News Reader</Title>
-            <List stories={stories} />
+            <InfiniteScroll
+              dataLength={stories.length}
+              next={this.fetchStories}
+              hasMore={hasMoreStories}
+              loader={<Loader />}
+              style={{
+                height: '100%',
+                overflow: 'visible',
+              }}
+            >
+              <List stories={stories} />
+            </InfiniteScroll>
           </Wrapper>
         </div>
       </ThemeProvider>
