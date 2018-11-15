7034cb200a0f649965399df575269c029a7aee19
Kristijan <kristija.junuzovski@codelogiq.com>
2018-11-15 13:27:12 +0100

    Updates

---
+++ precache-manifest.fe5472d0d70109d62e5af94269f8d4d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/hacker-news-clone/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a98814c031d2c8aeed84",
+    "url": "/hacker-news-clone/static/js/runtime~main.a98814c0.js"
+  },
+  {
+    "revision": "60b97202fb1cf4fa110c",
+    "url": "/hacker-news-clone/static/js/main.60b97202.chunk.js"
+  },
+  {
+    "revision": "ed675119690d35667650",
+    "url": "/hacker-news-clone/static/js/1.ed675119.chunk.js"
+  },
+  {
+    "revision": "0bb50170473c62c5c90a0d92915f6d2d",
+    "url": "/hacker-news-clone/index.html"
+  }
+];
